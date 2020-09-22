5b96c1a9d80ded77913a8ed90a01b6773fa1ed5f
RahulSChauhan50 <rahulschauhan50@gmail.com>
2020-09-22 20:05:17 +0530

    Homescren Added

---
+++ app/Shop/assets/screens/Home.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class Homescreen extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>Hello</Text>
+      </SafeAreaView>
+    );
+  }
+}
+export default Homescreen;
