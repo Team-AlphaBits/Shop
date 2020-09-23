588b9940256b5f7f7eab16bd0fd1165d050c0d31
Dezx20 <2009awektoppo@gmail.com>
2020-09-24 00:23:50 +0530

    Working on USER authen.
    Some here and their still needed and few thing need to be cleaned out.

--- Back_end/app.js
+++ Back_end/app.js
@@ -4,8 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const passport = require("passport");              
+require("./models/db");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
 
 var app = express();
 
@@ -18,6 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());     
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
