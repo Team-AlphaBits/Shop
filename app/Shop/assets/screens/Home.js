f4e2ef9e837d028613f1c2a852ab5921ed14748f
RahulSChauhan50 <rahulschauhan50@gmail.com>
2020-09-24 14:31:46 +0530

    login pageui cahnged

--- app/Shop/assets/screens/Home.js
+++ app/Shop/assets/screens/Home.js
@@ -6,15 +6,23 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
 } from 'react-native';
-
 
 class Homescreen extends Component {
   render() {
     return (
       <SafeAreaView>
         <View>
-        <Text>Hello</Text>
+          <Text>Hello</Text>
+          <View style={{width: 100, height: 50}}>
+            <Button
+              title="press me"
+              onPress={() => {
+                console.log('pressed');
+              }}
+            />
+          </View>
         </View>
       </SafeAreaView>
     );
