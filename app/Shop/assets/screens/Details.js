76bff9d3f524bfabbfa4692b635eb13c795f99d9
RahulSChauhan50 <rahulschauhan50@gmail.com>
2020-09-26 22:02:56 +0530

    debugged details view

--- app/Shop/assets/screens/Details.js
+++ app/Shop/assets/screens/Details.js
@@ -11,7 +11,7 @@ import {
   Dimensions,
 } from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
-import {Button} from 'react-native-paper';
+import {Button, Title, Paragraph} from 'react-native-paper';
 export default class Details extends React.Component {
   state = {
     active: 0,
@@ -26,39 +26,49 @@ export default class Details extends React.Component {
   };
   render() {
     return (
-      <View style={styles.MainContainer}>
-        <ScrollView>
-          <ScrollView
-            pagingEnabled
-            horizontal
-            onScroll={this.change}
-            showsHorizontalScrollIndicator={false}
-            style={{
-              width,
-              height: 300,
-              marginTop: 30,
-              alignSelf: 'flex-start',
-            }}>
-            {imageUri.map((image, index) => (
-              <Image source={image} style={styles.img} />
-            ))}
+      <SafeAreaView style={{flex: 1}}>
+        <View style={styles.MainContainer}>
+          <ScrollView>
+            <View style={{marginHorizontal: '3%'}}>
+              <ScrollView
+                pagingEnabled
+                horizontal
+                onScroll={this.change}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                  paddingVertical: '3%',
+                  width: imageUri.length * 100 + '%',
+                  height: 300,
+                  flexGrow: 1,
+                }}>
+                {imageUri.map((image, index) => (
+                  <Image
+                    source={image}
+                    style={styles.img}
+                    resizeMode="contain"
+                  />
+                ))}
+              </ScrollView>
+            </View>
+            <View style={styles.pagging}>
+              {imageUri.map((i, k) => (
+                <Text
+                  key={k}
+                  style={
+                    k == this.state.active
+                      ? styles.paggingActiveText
+                      : styles.paggingText
+                  }>
+                  ⬤
+                </Text>
+              ))}
+            </View>
+            <View style={styles.btnView}>
+              <Text style={styles.price}>₹ 24,999.00</Text>
+              <Text style={styles.avl}>In stock</Text>
+            </View>
           </ScrollView>
-          <View style={styles.pagging}>
-            {imageUri.map((i, k) => (
-              <Text
-                key={k}
-                style={
-                  k == this.state.active
-                    ? styles.paggingActiveText
-                    : styles.paggingText
-                }>
-                ⬤
-              </Text>
-            ))}
-          </View>
-          <View style={styles.btnView}>
-            <Text style={styles.price}>₹ 24,999.00</Text>
-            <Text style={styles.avl}>In stock</Text>
+          <View>
             <Button
               mode="contained"
               onPress={() => console.log('Pressed')}
@@ -72,8 +82,8 @@ export default class Details extends React.Component {
               Add to cart
             </Button>
           </View>
-        </ScrollView>
-      </View>
+        </View>
+      </SafeAreaView>
     );
   }
 }
@@ -84,21 +94,18 @@ const imageUri = [
   require('../images/img_3.jpg'),
   require('../images/img_4.jpg'),
 ];
-const {width} = Dimensions.get('screen');
-const height = (width * 100) / 60;
 
 const styles = StyleSheet.create({
-  MainContainer: {flex: 1, width, height},
+  MainContainer: {flex: 1},
   pagging: {
     flexDirection: 'row',
-    position: 'absolute',
-    marginTop: 320,
     alignSelf: 'center',
   },
   img: {
-    width,
-    height: 300,
-    resizeMode: 'cover',
+    width: null,
+    height: null,
+    flex: 1,
+    marginHorizontal: 10,
   },
   paggingText: {
     color: 'black',
@@ -114,13 +121,13 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   btn1: {
-    margin: '5%',
+    marginHorizontal: '5%',
     marginVertical: '2%',
     borderRadius: 8,
   },
   btn2: {
-    margin: '5%',
-    marginVertical: '2%',
+    marginHorizontal: '5%',
+    marginBottom: '2%',
     backgroundColor: 'blue',
     borderRadius: 8,
   },
@@ -134,7 +141,7 @@ const styles = StyleSheet.create({
     color: 'green',
     fontSize: 20,
     fontWeight: 'bold',
-    marginHorizontal: '5%',
+    marginStart: '5%',
     marginBottom: '5%',
   },
 });
