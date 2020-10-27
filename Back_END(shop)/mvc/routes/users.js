b1877489537ae69e1fa2fe17946ce497ab79ecc7
Dezx20 <2009awektoppo@gmail.com>
2020-10-27 21:01:15 +0530

    single product info add/ working on cart

--- Back_END(shop)/mvc/routes/users.js
+++ Back_END(shop)/mvc/routes/users.js
@@ -8,17 +8,30 @@ const productsCtrl = require("../controllers/product");
 const Product_data = require('../models/Product_data');
 
 
+
+///////////////////////////////////////////////////////////////
+/////////////////////USER ROUTES//////////////////////////////
+/////////////////////////////////////////////////////////////
+
 router.post("/register",usersCtrl.registerUser);
 
 router.post("/login",usersCtrl.loginUser);
 
 
+///////////////////////////////////////////////////////////////
+/////////////////////PRODUCTS ROUTES//////////////////////////
+/////////////////////////////////////////////////////////////
+
 router.get("/getSearchResults", productsCtrl.getSearchResults);
 
 router.get("/reset", productsCtrl.reset);
 
-router.get("/upload-products", productsCtrl.uploadProductsForm);
+router.post("/upload-products", productsCtrl.uploadProductsForm);
 
 router.get("/home", productsCtrl.sendProductData);
+
+router.get("/add-to-cart/:id", productsCtrl.addToCart);
+
+router.get("/product/:id", productsCtrl.getProductByID);
 
 module.exports = router;
