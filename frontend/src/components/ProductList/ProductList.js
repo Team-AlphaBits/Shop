1f9dba2dca2a286ba9595dee6f7fdc19f230080e
kn8rider <khemchandpatel784@gmail.com>
2020-11-15 16:24:48 +0530

    .

--- frontend/src/components/ProductList/ProductList.js
+++ frontend/src/components/ProductList/ProductList.js
@@ -6,7 +6,7 @@ import classes from "./ProductList.module.css";
 class ProductList extends Component {
   componentDidMount() {
     this.props.authCheckout();
-    if(this.props.history.location.pathname.split("/")[3] === "true"){
+    if (this.props.history.location.pathname.split("/")[3] === "true") {
       this.props.getProduct(this.props.history.location.pathname.split("/")[2]);
     }
   }
@@ -17,70 +17,97 @@ class ProductList extends Component {
       search: "?" + id,
     });
   };
-  discount= (price,arg,dis) =>{
-    let modifiedPrice = parseFloat(price.replace( /[^\d.]*/g,''));
-  if(arg === "newprice"){
-    return Math.floor(modifiedPrice - (dis * modifiedPrice)/100).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
-  }
-  else if(arg === "oldprice"){
-    let newprice = modifiedPrice.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
-    return newprice
-  }
-    else if(arg === "diff"){
-        return (modifiedPrice - Math.floor(modifiedPrice - (dis * modifiedPrice)/100)).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+  discount = (price, arg, dis) => {
+    let modifiedPrice = parseFloat(price.replace(/[^\d.]*/g, ""));
+    if (arg === "newprice") {
+      return Math.floor(modifiedPrice - (dis * modifiedPrice) / 100)
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+    } else if (arg === "oldprice") {
+      let newprice = modifiedPrice
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+      return newprice;
+    } else if (arg === "diff") {
+      return (
+        modifiedPrice - Math.floor(modifiedPrice - (dis * modifiedPrice) / 100)
+      )
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
     }
-}
+  };
   render() {
     console.log(this.props.Data);
     let cards = [];
-    if(this.props.history.location.pathname.split("/")[3] === "true"){
-    if (this.props.Data) {
-      for (let i = 0; i < this.props.Data.productData.length; i++) {
-        let des = Math.floor(Math.random() * (50)) + 1;
-        cards.push(
-          <div>
-            <div className={classes.container}>
-              <div className={classes.carousel_ctrl}>
-                <img
-                  src={this.props.Data.productData[i].home_image}
-                  className={classes.img}
-                  alt="some"
-                  onClick={() =>
-                    this.changeUrl(this.props.Data.productData[i]._id)
-                  }
-                />
-              </div>
-              <div className={classes.disc}>
-                <p
-                  className={classes.heading}
-                  onClick={() =>
-                    this.changeUrl(this.props.Data.productData[i]._id)
-                  }
-                >
-                  {this.props.Data.productData[i].short_desc}
-                </p>
-                <div className={classes.pricing}>
-                  {" "}
-                  <p className={classes.oldprc}>₹ {this.discount(this.props.Data.productData[i].price,"newprice",des)}</p>
-                  <strike className={classes.prc}>₹ {this.discount(this.props.Data.productData[i].price,"oldprice",des)}</strike>
-                <p className={classes.price}>Save: ₹{this.discount(this.props.Data.productData[i].price,"diff",des)} ({des}%)</p>
+    if (this.props.history.location.pathname.split("/")[3] === "true") {
+      if (this.props.Data) {
+        for (let i = 0; i < this.props.Data.productData.length; i++) {
+          let des = Math.floor(Math.random() * 50) + 1;
+          cards.push(
+            <div>
+              <div className={classes.container}>
+                <div className={classes.carousel_ctrl}>
+                  <img
+                    src={this.props.Data.productData[i].home_image}
+                    className={classes.img}
+                    alt="some"
+                    onClick={() =>
+                      this.changeUrl(this.props.Data.productData[i]._id)
+                    }
+                  />
                 </div>
-                <p className={classes.amount}>
+                <div className={classes.disc}>
+                  <p
+                    className={classes.heading}
+                    onClick={() =>
+                      this.changeUrl(this.props.Data.productData[i]._id)
+                    }
+                  >
+                    {this.props.Data.productData[i].short_desc}
+                  </p>
+                  <div className={classes.pricing}>
+                    {" "}
+                    <p className={classes.oldprc}>
+                      ₹{" "}
+                      {this.discount(
+                        this.props.Data.productData[i].price,
+                        "newprice",
+                        des
+                      )}
+                    </p>
+                    <strike className={classes.prc}>
+                      ₹{" "}
+                      {this.discount(
+                        this.props.Data.productData[i].price,
+                        "oldprice",
+                        des
+                      )}
+                    </strike>
+                    <p className={classes.price}>
+                      Save: ₹
+                      {this.discount(
+                        this.props.Data.productData[i].price,
+                        "diff",
+                        des
+                      )}{" "}
+                      ({des}%)
+                    </p>
+                  </div>
+                  <p className={classes.amount}>
                     FREE Delivery by <b>SHOP</b>{" "}
                   </p>
+                </div>
               </div>
+              <hr className={classes.line} />
             </div>
-            <hr className={classes.line} />
-          </div>
-        );
+          );
+        }
       }
-      }
-    }
-    else{
-      if(this.props.search){
-        console.log(this.props.search)
+    } else {
+      if (this.props.search) {
+        console.log(this.props.search);
         for (let i = 0; i < this.props.search.data.result.length; i++) {
-          let des = Math.floor(Math.random() * (50)) + 1;
+          let des = Math.floor(Math.random() * 50) + 1;
           cards.push(
             <div>
               <div className={classes.container}>
@@ -105,9 +132,31 @@ class ProductList extends Component {
                   </p>
                   <div className={classes.pricing}>
                     {" "}
-                    <p className={classes.oldprc}>₹ {this.discount(this.props.Data.productData[i].price,"newprice",des)}</p>
-                    <strike className={classes.prc}>₹ {this.discount(this.props.Data.productData[i].price,"oldprice",des)}</strike>
-                  <p className={classes.price}>Save: ₹{this.discount(this.props.Data.productData[i].price,"diff",des)} ({des}%)</p>
+                    <p className={classes.oldprc}>
+                      ₹{" "}
+                      {this.discount(
+                        this.props.Data.productData[i].price,
+                        "newprice",
+                        des
+                      )}
+                    </p>
+                    <strike className={classes.prc}>
+                      ₹{" "}
+                      {this.discount(
+                        this.props.Data.productData[i].price,
+                        "oldprice",
+                        des
+                      )}
+                    </strike>
+                    <p className={classes.price}>
+                      Save: ₹
+                      {this.discount(
+                        this.props.Data.productData[i].price,
+                        "diff",
+                        des
+                      )}{" "}
+                      ({des}%)
+                    </p>
                   </div>
                   <p className={classes.amount}>
                     FREE Delivery by <b>SHOP</b>{" "}
@@ -126,7 +175,7 @@ class ProductList extends Component {
 const mapStatetoProps = (state) => {
   return {
     Data: state.Login.catData,
-    search: state.Login.resultData
+    search: state.Login.resultData,
   };
 };
 const mapDispatchToprops = (dispatch) => {
@@ -134,7 +183,7 @@ const mapDispatchToprops = (dispatch) => {
     getProduct: (id) => {
       dispatch(actions.getBycatId(id));
     },
-    authCheckout: () => dispatch(actions.authCheckState())
+    authCheckout: () => dispatch(actions.authCheckState()),
   };
 };
 export default connect(mapStatetoProps, mapDispatchToprops)(ProductList);
