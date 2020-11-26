5005935759c86759479ad51b57a78bd968cf8804
kn8rider <khemchandpatel784@gmail.com>
2020-11-26 10:51:14 +0530

    .

--- frontend/src/components/details/details.js
+++ frontend/src/components/details/details.js
@@ -3,45 +3,50 @@ import classes from "./details.module.css";
 import { connect } from "react-redux";
 import * as actions from "../../Store/Action/index";
 import Carousel from "react-elastic-carousel";
-import Spinner from '../spinner/spinner'
-import Modal from '../Modal/Modal'
+import Spinner from "../spinner/spinner";
+import Modal from "../Modal/Modal";
+import carousel from "../carousel/carousel";
 
 class Details extends Component {
   componentDidMount() {
     this.props.getProduct(this.props.location.search.split("?")[1]);
   }
-  discount= (price,arg,dis) =>{
-    let modifiedPrice = parseFloat(price.replace( /[^\d.]*/g,''));
-  if(arg === "newprice"){
-    return Math.floor(modifiedPrice - (dis * modifiedPrice)/100).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
-  }
-  else if(arg === "oldprice"){
-    let newprice = modifiedPrice.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
-    return newprice
-  }
-    else if(arg === "diff"){
-        return (modifiedPrice - Math.floor(modifiedPrice - (dis * modifiedPrice)/100)).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+  discount = (price, arg, dis) => {
+    let modifiedPrice = parseFloat(price.replace(/[^\d.]*/g, ""));
+    if (arg === "newprice") {
+      return Math.floor(modifiedPrice - (dis * modifiedPrice) / 100)
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+    } else if (arg === "oldprice") {
+      let newprice = modifiedPrice
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+      return newprice;
+    } else if (arg === "diff") {
+      return (
+        modifiedPrice - Math.floor(modifiedPrice - (dis * modifiedPrice) / 100)
+      )
+        .toString()
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
     }
-}
-AddedtoCart = (id,stat) =>{
-  if(this.props.isAuthenticated && stat === "Add"){
-    this.props.addTocart(id)
-   }
-   else if(this.props.isAuthenticated && stat === "Buy"){
-    this.props.addTocart(id)
-    this.props.history.push({
-      pathname: "/cart"
-    });
-   }
-   else{
-    this.props.history.push({
-      pathname: "/login"
-    });
-  }
-  }
+  };
+  AddedtoCart = (id, stat) => {
+    if (this.props.isAuthenticated && stat === "Add") {
+      this.props.addTocart(id);
+    } else if (this.props.isAuthenticated && stat === "Buy") {
+      this.props.addTocart(id);
+      this.props.history.push({
+        pathname: "/cart",
+      });
+    } else {
+      this.props.history.push({
+        pathname: "/login",
+      });
+    }
+  };
   render() {
     let img = ["d-block w-100", classes.carimg];
-    let dis = Math.floor(Math.random() * (50)) + 1;
+    let dis = Math.floor(Math.random() * 50) + 1;
     let show = <Spinner />;
     if (this.props.DetailData) {
       let des = this.props.DetailData.productData.description.split(".");
@@ -85,21 +90,59 @@ AddedtoCart = (id,stat) =>{
               {this.props.DetailData.productData.short_desc}
             </p>
             <p className={classes.oldprc}>
-              M.R.P.: <strike>₹ {this.discount(this.props.DetailData.productData.price,"oldprice",dis)}</strike>
-            </p>  
+              M.R.P.:{" "}
+              <strike>
+                ₹{" "}
+                {this.discount(
+                  this.props.DetailData.productData.price,
+                  "oldprice",
+                  dis
+                )}
+              </strike>
+            </p>
             <p className={classes.amount}>
               Deal of the Day:{" "}
               <b className={classes.price}>
-                ₹ {this.discount(this.props.DetailData.productData.price,"newprice",dis)}.00
+                ₹{" "}
+                {this.discount(
+                  this.props.DetailData.productData.price,
+                  "newprice",
+                  dis
+                )}
+                .00
               </b>
             </p>
             <p className={classes.price2}>
-              You Save: <b className={classes.price}> ₹ {this.discount(this.props.DetailData.productData.price,"diff",dis)} (19%) </b>
+              You Save:{" "}
+              <b className={classes.price}>
+                {" "}
+                ₹{" "}
+                {this.discount(
+                  this.props.DetailData.productData.price,
+                  "diff",
+                  dis
+                )}{" "}
+                (19%){" "}
+              </b>
             </p>
             <b>Inclusive of all taxes</b>
             <p className={classes.avl}>In stock.</p>
-            <button className={classes.cartBtn} onClick={() => this.AddedtoCart(this.props.DetailData.productData._id,"Add")}>Add to Cart</button>
-            <button className={classes.buyBtn} onClick={() => this.AddedtoCart(this.props.DetailData.productData._id,"Buy")}>Buy Now</button>
+            <button
+              className={classes.cartBtn}
+              onClick={() =>
+                this.AddedtoCart(this.props.DetailData.productData._id, "Add")
+              }
+            >
+              Add to Cart
+            </button>
+            <button
+              className={classes.buyBtn}
+              onClick={() =>
+                this.AddedtoCart(this.props.DetailData.productData._id, "Buy")
+              }
+            >
+              Buy Now
+            </button>
             <p>
               Sold by{" "}
               <b className={classes.sold}>
@@ -115,8 +158,12 @@ AddedtoCart = (id,stat) =>{
         </div>
       );
     }
-    if(this.props.error){
-      show= <Modal modalclosed={() => this.props.errorNull()}>Some Error Occured...!</Modal>
+    if (this.props.error) {
+      show = (
+        <Modal modalclosed={() => this.props.errorNull()}>
+          Some Error Occured...!
+        </Modal>
+      );
     }
     return <>{show}</>;
   }
@@ -135,7 +182,7 @@ const mapDispatchToprops = (dispatch) => {
     },
     addTocart: (id) => dispatch(actions.addToCart(id)),
     authCheckout: () => dispatch(actions.authCheckState()),
-    errorNull: () => dispatch(actions.nullError())
+    errorNull: () => dispatch(actions.nullError()),
   };
 };
 export default connect(mapStatetoProps, mapDispatchToprops)(Details);
